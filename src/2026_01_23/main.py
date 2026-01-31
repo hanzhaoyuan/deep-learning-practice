2e9611d29fd7e3f46f0ae748ba3053db33fbe711
hanzhaoyuan <809170493@qq.com>
2026-01-31 18:01:18 +0800

    feat: 提交之前代码

--- src/2026_01_23/main.py
+++ src/2026_01_23/main.py
@@ -1,20 +1,24 @@
 from loader import load_data
-from plotting import plot_pred_2d
+from plotting import plot_pred_2d, plot_data_2d
 from numpy.random import default_rng
-from utils import dnn2_clas_new, train_w_value
+from utils import dnn3_clas, train_w_value
 
 
 def main():
-    (x, y_gt) = load_data("data_class_2d_convex_noisy.csv")
-    # print("x: ", x)
-    # print("y: ", y)
-    # plot_data_2d(x, y_gt)
-    num_neurons = 3
+    (x, y_gt) = load_data("data_class_2d_nonconvex.csv")
+    num_neurons_1 = 7
+    num_neurons_2 = 5
+
     rng = default_rng()
-    wa = rng.standard_normal(size=(2, num_neurons))  # Input weights layer 1
-    wb = rng.standard_normal(size=(num_neurons, 1))  # Input weights layer 2
-    (wa, wb) = train_w_value(x, y_gt, wa, wb)
-    plot_pred_2d(x, y_gt, y_p=dnn2_clas_new(wa, wb, x))
+    # Input weights layer 1
+    wa = rng.standard_normal(size=(2, num_neurons_1))
+    # Input weights layer 2
+    wb = rng.standard_normal(size=(num_neurons_1, num_neurons_2))
+    # Input weights layer 3
+    wc = rng.standard_normal(size=(num_neurons_2, 1))
+
+    (wa, wb, wc) = train_w_value(x, y_gt, wa, wb, wc)
+    plot_pred_2d(x, y_gt, y_p=dnn3_clas(wa, wb, wc, x))
 
 
 if __name__ == "__main__":
--- src/2026_01_23/utils.py
+++ src/2026_01_23/utils.py
@@ -7,6 +7,10 @@ def sigmoid(x):
     return 1 / (1 + exp(-x))
 
 
+def dnn3_clas(wa, wb, wc, x):
+    return sigmoid(sigmoid(sigmoid(x @ wa) @ wb) @ wc)
+
+
 def dnn2_clas(wa, wb, x):
     """Two-layer dense neural network for classification."""
     x_a = x  # Input layer 1
@@ -67,3 +71,47 @@ def train_w_value(x, y_gt, wa, wb):
         if i % 100 == 0:
             print(f"{i} y_p={y_p_selected[0, 0]:.2f} error={error[0, 0]:.2f}")
     return (wa, wb)
+
+
+def train_w_value(x, y_gt, wa, wb, wc):
+    num_samples = len(x)
+    num_train_iterations = 10 ** 5
+    eta = .1  # Learning rate
+    rng = default_rng()
+
+    for i in range(num_train_iterations):
+        # Select random sample.
+        selected = rng.integers(0, num_samples)
+        x_selected = reshape(x[selected], (1, -1))
+        y_gt_selected = reshape(y_gt[selected], (1, -1))
+
+        # Detailed neural network calculation.
+        x_selected_a = x_selected  # Input layer 1
+        p_a = x_selected_a @ wa  # Activation potential layer 1
+        y_selected_a = sigmoid(p_a)  # Output layer 1
+
+        x_selected_b = y_selected_a  # Input layer 2
+        p_b = x_selected_b @ wb  # Activation potential layer 2
+        y_selected_b = sigmoid(p_b)  # Output layer 2 (output neuron)
+
+        x_selected_c = y_selected_b # Input layer 3
+        p_c = x_selected_c @ wc # Activation potential layer 3
+        y_selected_c = sigmoid(p_c) # Output layer 3 (output neuron)
+        
+        y_p_selected = y_selected_c
+
+        # Update weights.
+        error = y_p_selected - y_gt_selected  # Calculate error
+
+        delta_c = error * d_sigmoid(p_c)
+        wc = wc - eta * delta_c * transpose(x_selected_c)
+
+        delta_b = error * d_sigmoid(p_b)
+        wb = wb - eta * delta_b * transpose(x_selected_b)
+
+        delta_a = sum(wb * delta_b, axis=1) * d_sigmoid(p_a)
+        wa = wa - eta * delta_a * transpose(x_selected_a)
+
+        if i % 100 == 0:
+            print(f"{i} y_p={y_p_selected[0, 0]:.2f} error={error[0, 0]:.2f}")
+    return (wa, wb, wc)
